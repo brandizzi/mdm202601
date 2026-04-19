042cf1a4695fddd6377a298107d2b840292126d9
Adam Victor Nazareth Brandizzi <adam@brandizzi.com.br>
2026-04-18 23:13:19 -0300

    Basic pivoting (not working for cpu)

--- load.py
+++ load.py
@@ -2,30 +2,37 @@
 import glob
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, date_trunc, expr
+from pyspark.sql.functions import col, date_trunc, expr, regexp_extract
 
 import typer
 
-# Load a single CSV file
 def main(memory_path, busy_path, cpu_path, request_path):
-    # Initialize Spark Session
     spark = SparkSession.builder.appName("CSVLoader").getOrCreate()
 
     busy_df = load_csvs(spark, busy_path)
-    busy_df.show(truncate=30)
-    print(busy_df.count())
-
     memory_df = load_csvs(spark, memory_path)
-    memory_df.show(truncate=30)
-    print(memory_df.count())
-
     cpu_df = load_csvs(spark, cpu_path)
-    cpu_df.show(truncate=30)
-    print(cpu_df.count())
-
     request_df = load_csvs(spark, request_path)
-    request_df.show(truncate=30)
-    print(request_df.count())
+
+    combined_df = pivot_metric(busy_df) \
+        .join(pivot_metric(memory_df), on=['start_time', 'pod_name'], how='outer') \
+        .join(pivot_metric(cpu_df), on=['start_time', 'pod_name'], how='outer') \
+        .join(pivot_metric(request_df), on=['start_time', 'pod_name'], how='outer') \
+        .sort('start_time', 'pod_name')
+
+    combined_df.show(truncate=40)
+    print(combined_df.count())
+
+
+def pivot_metric(df):
+    metric_name = df.select(
+        regexp_extract(col('metric_type'), r'/([^/]+)/[^/]+$', 1)
+    ).first()[0]
+    return df.select(
+        col('start_time'),
+        col('metric:pod').alias('pod_name'),
+        col('value').alias(metric_name)
+    )
 
 def load_csvs(spark, dir_path):
     paths = glob.glob(dir_path + "/*.csv")
@@ -37,6 +44,7 @@ def load_csvs(spark, dir_path):
         .drop("value_type") \
         .drop("resource_type") \
         .drop("end_time") \
+        .drop("metric:area") \
         .drop("metric:top_level_controller_name") \
         .drop("metric:top_level_controller_type") \
         .drop("resource:cluster") \
