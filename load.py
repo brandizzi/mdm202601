7f9b02788e468889948c9c71ddfb2b928a23d475
Adam Victor Nazareth Brandizzi <adam@brandizzi.com.br>
2026-04-18 23:21:44 -0300

    Replicate the CPU value because it is only picked every 10 minutes

--- load.py
+++ load.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import glob
 
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, date_trunc, expr, regexp_extract
+from pyspark.sql import SparkSession, Window
+from pyspark.sql.functions import col, date_trunc, expr, last, regexp_extract
 
 import typer
 
@@ -14,11 +14,16 @@ def main(memory_path, busy_path, cpu_path, request_path):
     cpu_df = load_csvs(spark, cpu_path)
     request_df = load_csvs(spark, request_path)
 
+    cpu_col = pivot_metric(cpu_df).columns[2]
+
     combined_df = pivot_metric(busy_df) \
         .join(pivot_metric(memory_df), on=['start_time', 'pod_name'], how='outer') \
         .join(pivot_metric(cpu_df), on=['start_time', 'pod_name'], how='outer') \
         .join(pivot_metric(request_df), on=['start_time', 'pod_name'], how='outer') \
         .sort('start_time', 'pod_name')
+
+    pod_time_window = Window.partitionBy('pod_name').orderBy('start_time').rowsBetween(Window.unboundedPreceding, 0)
+    combined_df = combined_df.withColumn(cpu_col, last(col(cpu_col), ignorenulls=True).over(pod_time_window))
 
     combined_df.show(truncate=40)
     print(combined_df.count())
@@ -40,6 +45,7 @@ def load_csvs(spark, dir_path):
         .option("header", "True") \
         .option("inferSchema", "True") \
         .load(paths) \
+        .filter(col('metric_type') != 'metric_type') \
         .drop("metric_kind") \
         .drop("value_type") \
         .drop("resource_type") \
@@ -57,8 +63,8 @@ def load_csvs(spark, dir_path):
             'start_time',
             (date_trunc('minute', col('start_time')) - expr("(minute(start_time) % 2) * interval 1 minute")).cast('timestamp')
         ) \
-        .sort('start_time') \
-        .distinct() \
+        .sort('start_time', ascending=True) \
+        .distinct()
 
 
 if __name__ == "__main__":
